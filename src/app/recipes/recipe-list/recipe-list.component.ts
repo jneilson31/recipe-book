0d6a2ff8a2dee8748e9e4500f241a10addfc41e5
Jeremy <jeremyneilson31@gmail.com>
2018-11-26 16:53:57 -0700

    Latest recipe book updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Bacon Noodle Soup', 'Bacon ipsum dolor amet leberkas corned.', 'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg'),
+    new Recipe('Bacon Noodle Soup', 'Bacon ipsum dolor amet leberkas corned.', 'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg')
+  ];
 
   constructor() { }
 
