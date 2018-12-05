6ce51889f399eb189a8f04582ac889da3127d89f
Jeremy <jeremyneilson31@gmail.com>
2018-12-05 11:17:49 -0700

    latest changes to recipe book

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Bacon Noodle Soup', 'Bacon ipsum dolor amet leberkas corned.', 'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg'),
-    new Recipe('Bacon Noodle Soup', 'Bacon ipsum dolor amet leberkas corned.', 'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg')
+    new Recipe('Chicken Roll Ups', 'Breaded chicken roll up description.', 'https://images-gmi-pmc.edge-generalmills.com/95733b94-0838-4a4e-81fc-ae603b2e4738.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe : Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
