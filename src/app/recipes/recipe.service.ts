1c815251517f68d63a34c9bc7b07e19f3bba7cdd
Jeremy <jeremyneilson31@gmail.com>
2019-01-04 10:28:10 -0700

    routing start, section 12 lecture 150

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,41 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService {
 
     selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Bacon Noodle Soup', 'Bacon ipsum dolor amet leberkas corned.', 'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg'),
-        new Recipe('Chicken Roll Ups', 'Breaded chicken roll up description.', 'https://images-gmi-pmc.edge-generalmills.com/95733b94-0838-4a4e-81fc-ae603b2e4738.jpg')
+        new Recipe(
+
+            'Bacon Noodle Soup',
+            'Bacon ipsum dolor amet leberkas corned.',
+            'https://c1.staticflickr.com/8/7048/8688987851_6603b30c22_b.jpg',
+            [
+                new Ingredient('Bacon', 5),
+                new Ingredient('Noodles', 1)
+            ]),
+        new Recipe('Chicken Roll Ups',
+            'Breaded chicken roll up description.',
+            'https://images-gmi-pmc.edge-generalmills.com/95733b94-0838-4a4e-81fc-ae603b2e4738.jpg',
+            [
+                new Ingredient('Chicken', 3),
+                new Ingredient('Rolls', 8)
+            ]),
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 
 
