2f625b4f3158a48e2377b06a60309268b2adf231
Jeremy <jeremyneilson31@gmail.com>
2019-01-14 15:17:10 -0700

    latest recipe book changes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,13 +2,13 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class Recipe {
     public name: string;
-    public description: string;
+    public recipeDescription: string;
     public imagePath: string;
     public ingredients: Ingredient[];
 
 constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
-    this.description = desc;
+    this.recipeDescription = desc;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
     }
